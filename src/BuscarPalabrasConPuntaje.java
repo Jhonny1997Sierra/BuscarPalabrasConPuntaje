b5212c7accac340a9fb68bbc53fdaa79c8169ff6
Jhonny1997Sierra <134241392+Jhonny1997Sierra@users.noreply.github.com>
2023-07-27 19:38:12 -0500

    segunda version con modificaciones generales

---
+++ src/BuscarPalabrasConPuntaje.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BuscarPalabrasConPuntaje {
+
+    public static void main(String[] args) {
+        // Paso 1: Solicitar al usuario la cantidad de palabras que desea buscar.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("BUSCADOR DE PALABRAS USUALMENTE USADAS EN EL PISHING ");
+        System.out.println("Instruccion general: El programa no continuara hasta ingresar una palabra de la lista ");
+        System.out.print("Ingrese la cantidad de palabras que desea buscar: ");
+        int cantidadPalabras = scanner.nextInt();
+        scanner.nextLine(); // Consumimos el salto de línea después del número.
+
+        // Paso 2: Leer el archivo .txt que contiene las palabras y sus puntajes.
+        Map<String, Integer> palabrasYPuntajes = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("palabras.txt"))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(" ");
+                if (partes.length == 2) {
+                    String palabra = partes[0].toLowerCase(); // Convertir la palabra a minúsculas
+                    int puntaje = Integer.parseInt(partes[1]);
+                    palabrasYPuntajes.put(palabra, puntaje);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo.");
+            e.printStackTrace();
+            return;
+        }
+
+        // Paso 3: Buscar las palabras solicitadas por el usuario y mostrar su puntaje.
+        int sumaPuntajes = 0;
+        Map<String, Integer> palabrasEncontradas = new HashMap<>();
+        for (int i = 0; i < cantidadPalabras; i++) {
+            System.out.print("Ingrese la palabra #" + (i + 1) + ": ");
+            String palabra = scanner.nextLine().toLowerCase(); // Convertir la palabra ingresada a minúsculas
+
+            while (!palabrasYPuntajes.containsKey(palabra)) {
+                System.out.println("La palabra " + palabra + " no fue encontrada. Intente nuevamente.");
+                System.out.print("Ingrese la palabra #" + (i + 1) + ": ");
+                palabra = scanner.nextLine().toLowerCase(); // Convertir la palabra ingresada a minúsculas
+            }
+
+            int puntajeEncontrado = palabrasYPuntajes.get(palabra);
+            System.out.println("La palabra encontrada es " + palabra + " y el puntaje es " + puntajeEncontrado);
+            sumaPuntajes += puntajeEncontrado;
+
+            // Guardar la palabra encontrada y su puntaje para mostrar en la tabla final.
+            palabrasEncontradas.put(palabra, puntajeEncontrado);
+        }
+
+        // Paso 4: Mostrar la tabla con las palabras encontradas y la suma total de puntajes.
+        System.out.println("\nTabla de palabras encontradas:");
+        System.out.println("--------------------------------");
+        System.out.printf("| %15s | %7s |\n", "Palabra", "Puntaje");
+        System.out.println("--------------------------------");
+        for (Map.Entry<String, Integer> entry : palabrasEncontradas.entrySet()) {
+            String palabra = entry.getKey();
+            int puntaje = entry.getValue();
+            System.out.printf("| %15s | %7d |\n", palabra, puntaje);
+        }
+        System.out.println("--------------------------------");
+        System.out.printf("| %15s | %7d |\n", "Suma total", sumaPuntajes);
+        System.out.println("--------------------------------");
+    }
+}
